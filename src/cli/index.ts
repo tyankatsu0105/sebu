31c680d03689f05d918f8e18e795e1db81f1acb7
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-06-03 00:25:02 +0900

    feat: add example on cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -18,8 +18,10 @@ program
     "after",
     `
 Example:
-$ sebu --current=1.0.0 --next=1.0.1 --source=docs/README.md
+$ sebu --current=1.0.0 --next=1.0.1 --source=docs/README.md -w
 $ sebu --current=1.0.0 --next=1.0.1 --source="docs/**/*.md"
+$ sebu --current=1.0.0 --major --source=package.json
+$ sebu --current=1.0.0 --next=1.0.1 --source="docs/**/*.md" -o sebu.json
 `
   )
   .parse(process.argv);
